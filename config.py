436746529b633c36d93c68df06824cf3d0bb1c0f
zack-bitcoin <zack.bitcoin@gmail.com>
2014-03-15 14:55:10 -0700

    automatically mine

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ peers_list=['http://www.sbb.tc:82/info?{}',
 listen_port=80
 gui_port=8090
 brain_wallet="password"#<----if you are mining. this is the brain wallet that gets paid to.
-hashes_till_check=0
+hashes_till_check=1
